019e53c902e01d517e6c20b364143ac6c71f8f46
NickJC01 <nchaudoir@gmail.com>
2024-10-20 12:45:23 -0700

    Pushing Basic Cube

--- Assignment-3/BasicCube.js
+++ Assignment-3/BasicCube.js
@@ -2,16 +2,114 @@
 //
 //  BasicCube.js
 //
-//  A cube defined of 12 triangles
+//  A cube defined of 12 triangles (36 vertices)
 //
 
 class BasicCube {
     constructor(gl, vertexShader, fragmentShader) {
+        vertexShader ||= `
+            in vec4 aPosition;
+            in vec4 aColor;
 
-        // let program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
+            uniform mat4 P;
+            uniform mat4 MV;
+
+            out vec4 vColor;
+
+            void main() {
+                gl_Position = P * MV * aPosition;
+                vColor = aColor;
+            }
+        `;
+
+        fragmentShader ||= `
+            precision mediump float;
+            in vec4 vColor;
+            out vec4 fColor;
+
+            void main() {
+                fColor = vColor;
+            }
+        `;
+
+        let program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
+
+        const positions = new Float32Array([
+            // Front face
+            -0.5, -0.5,  0.5,
+             0.5, -0.5,  0.5,
+             0.5,  0.5,  0.5,
+            -0.5, -0.5,  0.5,
+             0.5,  0.5,  0.5,
+            -0.5,  0.5,  0.5,
+
+            // Back face
+            -0.5, -0.5, -0.5,
+            -0.5,  0.5, -0.5,
+             0.5,  0.5, -0.5,
+            -0.5, -0.5, -0.5,
+             0.5,  0.5, -0.5,
+             0.5, -0.5, -0.5,
+
+            // Top face
+            -0.5,  0.5, -0.5,
+            -0.5,  0.5,  0.5,
+             0.5,  0.5,  0.5,
+            -0.5,  0.5, -0.5,
+             0.5,  0.5,  0.5,
+             0.5,  0.5, -0.5,
+
+            // Bottom face
+            -0.5, -0.5, -0.5,
+             0.5, -0.5, -0.5,
+             0.5, -0.5,  0.5,
+            -0.5, -0.5, -0.5,
+             0.5, -0.5,  0.5,
+            -0.5, -0.5,  0.5,
+
+            // Right face
+             0.5, -0.5, -0.5,
+             0.5,  0.5, -0.5,
+             0.5,  0.5,  0.5,
+             0.5, -0.5, -0.5,
+             0.5,  0.5,  0.5,
+             0.5, -0.5,  0.5,
+
+            // Left face
+            -0.5, -0.5, -0.5,
+            -0.5, -0.5,  0.5,
+            -0.5,  0.5,  0.5,
+            -0.5, -0.5, -0.5,
+            -0.5,  0.5,  0.5,
+            -0.5,  0.5, -0.5
+        ]);
+
+        // Function to generate random colors for psychidelic mode
+        const generateRandomColor = () => {
+            return [Math.random(), Math.random(), Math.random(), 1.0];
+        };
+
+        // Generate random colors for 36 vertices
+        let colors = [];
+        for (let i = 0; i < 36; i++) {
+            colors = colors.concat(generateRandomColor());
+        }
+
+        colors = new Float32Array(colors);
+
+        let aPosition = new Attribute(gl, program, "aPosition", positions, 3, gl.FLOAT);
+        let aColor = new Attribute(gl, program, "aColor", colors, 4, gl.FLOAT);
 
         this.draw = () => {
-            // program.use();
+            program.use();
+
+            aPosition.enable();
+            aColor.enable();
+
+            gl.drawArrays(gl.TRIANGLES, 0, 36);
+
+            aPosition.disable();
+            aColor.disable();
         };
     }
 };
